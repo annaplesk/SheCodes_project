ed707f33894ef1cd69f23b7d08ae098f2736e1f2
annaplesk <hannakulchytska@gmail.com>
2022-09-05 15:41:42 +0300

    Fixed API url

--- js/script.js
+++ js/script.js
@@ -50,8 +50,8 @@ function displayForecast() {
 
 function getCoordinates(coordinates) {
   let apiKey = "c95d60a1e3adbeb286133f1ebebc2579";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}$units=metric`;
-  console.log(apiUrl);
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
 }
 function showTemp(response) {
   let currentTemp = document.querySelector("#temp");
